c2e015eaed9499ec31df77da21a5a0ded12f4b73
Jason Ziemba <jason@ziemba.net>
2022-06-02 15:42:43 -0400

    Expanding variable list

--- config/fixConfig.sh
+++ config/fixConfig.sh
@@ -5,6 +5,8 @@ cp -r /tmp/powerdns /etc/
 sed -i "s|&&IPLIST&&|$iplist|g" /etc/powerdns/pdns.conf
 sed -i "s|&&IPLIST2&&|$iplist2|g" /etc/powerdns/pdns.conf
 sed -i "s|&&APIKEY&&|$apikey|g" /etc/powerdns/pdns.conf
+sed -i "s|&&AXFRIPS&&|$axfrips|g" /etc/powerdns/pdns.conf
+sed -i "s|&&IPMASTER&&|$ipmaster|g" /etc/powerdns/pdns.conf
 
 sed -i "s|&&DBSERVER&&|$dbServer|g" /etc/powerdns/pdns.d/pdns.local.gmysql.conf
 sed -i "s|&&DBNAME&&|$dbName|g" /etc/powerdns/pdns.d/pdns.local.gmysql.conf
